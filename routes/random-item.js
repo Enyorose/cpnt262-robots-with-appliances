9a3864921f9da83738dbca28be97ee0ab48e66ec
Jessica.W <jessica.watson1@live.com>
2021-11-04 14:22:42 -0600

    added api information

---
+++ routes/random-item.js
@@ -0,0 +1,15 @@
+
+
+const randomItem = array => {
+  const length = array.length;
+  const randNumber = Math.random();
+  const arrayRatio = randNumber * length;
+  const lowInteger = Math.floor(arrayRatio);
+  const item = array[lowInteger];
+
+  console.log(randNumber, arrayRatio, lowInteger)
+  return item
+}
+
+
+module.exports = randomItem
