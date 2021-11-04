77eed6893be48d28f0f896abbb7b7e7c489a4c36
Jessica.W <jessica.watson1@live.com>
2021-11-04 14:51:04 -0600

    fixed module path-displaying on local3000

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 
-const gallery = require('./gallery')
+const gallery = require('../models/seeds/images')
 const randomItem = require('./random-item')
 
 /*****************/
