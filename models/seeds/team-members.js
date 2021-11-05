a4a26938395a5394ec664d82c143feb37f2738b7
Jessica.W <jessica.watson1@live.com>
2021-11-05 14:48:14 -0600

    added team member data

--- models/seeds/team-members.js
+++ models/seeds/team-members.js
@@ -1,19 +1,30 @@
 const teamMembers = [
   {
     name: 'Jessica Watson',
-    profilePic:,
-    title: 'Project coordinator'
-    bio
-    github
-
+    profilePic:'',
+    title: 'Project coordinator',
+    bio: 'I am a mom of a wonderful boy as well as a photographer and an aspiring web developer.',
+    github: ''
   },
   {
-
+    name: 'David Ha',
+    profilePic:'',
+    title: 'Front End Specialist',
+    bio: 'Automotive RS Journeyman -> DJ -> Gamer -> Web Developer??',
+    github: 'https://github.com/boostha'
   },
   {
-
+    name: 'Jared Herman',
+    profilePic:'',
+    title: 'Data Manager',
+    bio: 'Front end and back end Web Developer',
+    github: 'https://github.com/jaredherman'
   },
   {
-
+    name: 'John paul Adobas',
+    profilePic: '',
+    title: 'Front End JS Expert',
+    bio: '',
+    github: 'https://github.com/jaypee06'
   }
 ]
