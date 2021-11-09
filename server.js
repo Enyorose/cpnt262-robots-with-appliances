08e94848d60029203f5b81f8ff07b9b418be2953
Jessica.W <jessica.watson1@live.com>
2021-11-09 13:55:09 -0700

    started server

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
+app.use(express.static('public'))
 
+
+
+
+/****************************/
+/* Handle 404, start server */
+/****************************/
+
+// Handle 404 errors with middleware
+app.use(function(request, response) {
+
+  // If path starts with `/api`, send JSON 404
+  if (request.url.startsWith('/api')) {
+
+    response.status(404)
+    response.send({error: 'File Not Found'})
+
+  } else {
+  
+    // else send HTML 404
+    response.status(404)
+    response.send('<h1>404: File Not Found</h1>')
+
+  }
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
 
