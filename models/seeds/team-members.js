d57b4e50acfaed2aaa44e1cc27145f87b755da0e
Jessica.W <jessica.watson1@live.com>
2021-11-11 12:01:46 -0700

    added team members

--- models/seeds/team-members.js
+++ models/seeds/team-members.js
@@ -1,15 +1,15 @@
 const teamMembers = [
   {
     name: 'Jessica Watson',
-    profilePic:'',
+    profilePic:'https://avatars.githubusercontent.com/u/90333235?s=400&u=ad61197220d6ab9bb8262187e342c3f4514f0da4&v=4',
     description:'photo of jessica watson',
     title: 'Project coordinator',
     bio: 'I am a mom of a wonderful boy as well as a photographer and an aspiring web developer.',
-    github: ''
+    github: 'https://github.com/Enyorose'
   },
   {
     name: 'David Ha',
-    profilePic:'',
+    profilePic:'https://avatars.githubusercontent.com/u/90293527?v=4',
     description: 'photo of david ha',
     title: 'Front End Specialist',
     bio: 'Automotive RS Journeyman -> DJ -> Gamer -> Web Developer??',
@@ -17,7 +17,7 @@ const teamMembers = [
   },
   {
     name: 'Jared Herman',
-    profilePic:'',
+    profilePic:'https://avatars.githubusercontent.com/u/90302843?v=4',
     description:'photo of jared herman',
     title: 'Data Manager',
     bio: 'Front end and back end Web Developer',
@@ -25,10 +25,10 @@ const teamMembers = [
   },
   {
     name: 'John paul Adobas',
-    profilePic: '',
+    profilePic: 'https://avatars.githubusercontent.com/u/90332320?v=4',
     description: 'photo of john paul abodas',
     title: 'Front End JS Expert',
-    bio: '',
+    bio: 'Aspiring full stack developer',
     github: 'https://github.com/jaypee06'
   }
 ]
