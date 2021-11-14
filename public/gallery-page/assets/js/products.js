181b54d4f61d181c0509cff8ef003580291ba619
David Ha <David.ha0908@gmail.com>
2021-11-13 19:05:46 -0700

    trying fetch

--- public/gallery-page/assets/js/products.js
+++ public/gallery-page/assets/js/products.js
@@ -28,6 +28,36 @@ fetch('https://robot-design.herokuapp.com/api/images')
     `
   });
 
+  fetch('https://robot-design.herokuapp.com/api/images/:id')
+  .then(function(response){
+    // JSON that is returned from the server must be converted to a JS object asynchronously.
+    if (!response.ok) {
+      throw new Error('Not 200 OK');
+    }
+    return response.json();
+  })
+  .then(function(gallery){
+    let output = " ";
+    gallery.forEach((products) => {
+        output += `
+        <figure>
+        <img src="${products.imageSrc}" alt="${products.description}" width="450">
+        <figcaption>${products.title}</figcaption>
+        </figure>
+        `
+      })
+
+      document.querySelector('.single-item').innerHTML = output;
+  })
+  .catch(function(err){
+    // An error or `reject` from any of the above `.then()` blocks will end up here.
+    const error = document.querySelector('.error')
+    error.innerHTML = `
+    <h1>Turn back this page is not available</h1>
+    <h2>404 Page not found</h2>
+    `
+  });
+
 const hamburgerMenu = document.querySelector('.hamburger')
 const closeMenu = document.querySelector('.close')
 const navList = document.querySelector('.nav-list')
